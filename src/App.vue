fc1afa52060705c10fac5a781fd31ba3d8c0c91d
dhilim <dhieliem@gmail.com>
2021-10-21 06:05:09 +0700

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    </div> -->
+    <!-- <router-view /> -->
+    <Login />
+    <Footer />
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Footer from "./components/Footer.vue";
+import Login from "./components/Login.vue";
+@Component({
+  components: {
+    Login,
+    Footer,
+  },
+})
+export default class App extends Vue {}
+</script>
+
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -28,5 +43,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
